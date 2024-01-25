6b81c6ee401b713a3367fae63401033b2f82744a
ANTONIOALGMAR <antonioalgmar@gmail.com>
2024-01-25 13:46:44 -0300

    salvar login e atualizar

--- src/main/java/dao/DAOUsuarioRepository.java
+++ src/main/java/dao/DAOUsuarioRepository.java
@@ -19,6 +19,8 @@ public class DAOUsuarioRepository {
 	
 	public ModelLogin gravarUsuario(ModelLogin objeto) throws Exception {
 		
+		if (objeto.isNovo()) {/*Grava um novo*/
+		
 		String sql = "INSERT INTO model_login(login, senha, nome, email)  VALUES (?, ?, ?, ?);";
 		PreparedStatement preparedSql = connection.prepareStatement(sql);
 		
@@ -30,6 +32,22 @@ public class DAOUsuarioRepository {
 		preparedSql.execute();
 		
 		connection.commit();
+		
+		}else {
+			String sql = "UPDATE model_login SET login=?, senha=?, nome=?, email=? WHERE id =  "+objeto.getId()+";";
+			
+			PreparedStatement prepareSql = connection.prepareStatement(sql);
+			
+			prepareSql.setString(1, objeto.getLogin());
+			prepareSql.setString(2, objeto.getSenha());
+			prepareSql.setString(3, objeto.getNome());
+			prepareSql.setString(4, objeto.getEmail());
+			
+			prepareSql.executeUpdate();
+			
+			connection.commit();
+			
+		}
 		
 		
 		return this.consultaUsuario(objeto.getLogin());
@@ -44,15 +62,15 @@ public class DAOUsuarioRepository {
 		
 		PreparedStatement statement = connection.prepareStatement(sql);
 		
-		ResultSet resultado =  statement.executeQuery();
+		ResultSet resutlado =  statement.executeQuery();
 		
-		while (resultado.next()) /*Se tem resultado*/ {
+		while (resutlado.next()) /*Se tem resultado*/ {
 			
-			modelLogin.setId(resultado.getLong("id"));
-			modelLogin.setEmail(resultado.getString("email"));
-			modelLogin.setLogin(resultado.getString("login"));
-			modelLogin.setSenha(resultado.getString("senha"));
-			modelLogin.setNome(resultado.getString("nome"));
+			modelLogin.setId(resutlado.getLong("id"));
+			modelLogin.setEmail(resutlado.getString("email"));
+			modelLogin.setLogin(resutlado.getString("login"));
+			modelLogin.setSenha(resutlado.getString("senha"));
+			modelLogin.setNome(resutlado.getString("nome"));
 		}
 		
 		
@@ -60,18 +78,18 @@ public class DAOUsuarioRepository {
 		
 	}
 	
+	
+	
 	public boolean validarLogin(String login) throws Exception {
-		String sql = "select count(1) > 0 from model_login where upper(login) = upper('"+login+"');";
+		String sql = "select count(1) > 0 as existe from model_login where upper(login) = upper('"+login+"');";
 		
-		PreparedStatement statement = connection.prepareStatement(sql);
+        PreparedStatement statement = connection.prepareStatement(sql);
 		
-		ResultSet resultado =  statement.executeQuery();
+		ResultSet resutlado =  statement.executeQuery();
 		
-		resultado.next() ;// PARA ENTRAR NOS RESULTADOS DO SQL
-		return resultado.getBoolean("existe");
-		}
+		resutlado.next();/*Pra ele entrar nos resultados do sql*/
+		return resutlado.getBoolean("existe");
 		
-		
-	
-	
+	}
+
 }
--- src/main/java/model/ModelLogin.java
+++ src/main/java/model/ModelLogin.java
@@ -10,6 +10,17 @@ public class ModelLogin implements Serializable {
 	private String email;
 	private String login;
 	private String senha;
+	
+	public boolean isNovo() {
+		
+		if (this.id == null) {
+			return true;  // INSERIR NOVO
+		}else if(this.id != null && this.id >0){
+			return false; // ATUALIZAR
+		}
+		
+		return id == null;
+	}
 
 	public String getLogin() {
 		return login;
